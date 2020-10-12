4f685ab42b903478f1adb93e7b20781320e37c5e
YoungAncient <68391309+YoungAncient@users.noreply.github.com>
2020-10-11 20:08:11 -0400

    tested api call successfully

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
 var apiKey = "90b29c37cec531de43800537a322384a";
+var searhcBtnEl = $(".searchBtn");
+//var searchInput = $(".searchInput");
+var searchInput = "Toronto";
+
+function getWeather (searchInput) {
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&APPID=" + apiKey);
+};
